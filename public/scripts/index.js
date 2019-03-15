6cd7486dc2b7d71ed87b8ebfc5ed4111dfe7d809
MariliaElia <melia1@sheffield.ac.uk>
2019-03-15 14:00:25 +0000

    Title bar

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,8 +2,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("heading").style.fontSize = "30px";
+        document.getElementById("title").style.fontSize = "30px";
     } else {
-        document.getElementById("heading").style.fontSize = "90px";
+        document.getElementById("title").style.fontSize = "90px";
     }
 }
