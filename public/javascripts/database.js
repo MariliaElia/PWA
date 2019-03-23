d142fdd4980e7fb635df0d0494fd444741237c2e
Cesim <fccivisilli1@sheffield.ac.uk>
2019-03-23 18:28:57 +0000

    - removed service worker - can create event and gets added to the indexed db

--- public/javascripts/database.js
+++ public/javascripts/database.js
@@ -1,131 +1,29 @@
-////////////////// DATABASE //////////////////
-// the database receives from the server the following structure
-/** class Event{
- * constructor (title, description, date, creator) {
- *   this.title = title;
- *   this.desciption= description;
- *   this.date= date;
- *   this.creator= creator;
- * }
- *}
- *  NOTE! the database is implemented with localstorage. This is not ideal as localStorage
- *  is blocking. It would be best to implement it using indexedDB
- */
-
-var dbPromise;
-
 function initDatabase() {
-    dbPromise = idb.openDb('photofest-db', 1,
-        function(upgradeDb) {
-            console.log('making a new object store');
-            if (!upgradeDb.objectStoreNames.contains('event')) {
-                var eventOS = upgradeDb.createObjectStore('event', {keyPath: 'title'});
-                eventOS.createIndex('id', 'id', {unique: true});
-                myevent = {title: 'my event 1', id: '1'};
-                myevent2 = {title: 'my event 2', id: '2'};
-                storeCachedData(myevent);
-                storeCachedData(myevent2);
-                console.log('tried to add an event');
-            }
-        });
+    console.log('called initDatabase()');
+    dbPromise = idb.openDb('PHOTOFEST_DB', 1, function (upgradeDb) {
+        if (!upgradeDb.objectStoreNames.contains('EVENT_OS')) {
+            var eventDb = upgradeDb.createObjectStore('EVENT_OS', {keyPath: 'id', autoIncrement: true, unique: true});
+            eventDb.createIndex('title', 'title', {unique: false});
+            eventDb.createIndex('description', 'description', {unique: false});
+            eventDb.createIndex('date', 'date', {unique: false});
+            eventDb.createIndex('location', 'location', {unique: false});
+        }
+    });
+    console.log('created object store')
 }
 
-
-/**
- * it saves the event in indexed db
- * @param eventObject
- */
-function storeCachedData(eventObject) {
-    //localStorage.setItem(event, JSON.stringify(eventDetails));
+function storeCachedData(newObject, objectStore) {
+    console.log('inserting: '+JSON.stringify(newObject));
     if (dbPromise) {
         dbPromise.then(async db => {
-            var tx = db.transaction('event', "readwrite");
-            var store = tx.objectStore('event');
-            await store.put(eventObject); //await is necessary to return a promise
+            var tx = db.transaction(objectStore, 'readwrite');
+            var store = tx.objectStore(objectStore);
+            await store.put(newObject);
             return tx.complete;
         }).then(function () {
-            console.log('added item to the event store' + JSON.stringify(eventObject));
+            console.log('added item to the store! '+ JSON.stringify(newObject));
         }).catch(function (error) {
-            console.log('could not add');
+            alert('could not add object to object store')
         });
     }
-    else {
-        console.log('could not db promise')
-    }
-}
-
-function getLoginData(loginObject) {
-    if (dbPromise) {
-        dbPromise.then(function (db) {
-            console.log('fetching: '+login);
-            var tx = db.transaction(LOGIN_STORE_NAME, 'readonly');
-            var store = tx.objectStore(LOGIN_STORE_NAME);
-            var index = store.index('userId');
-            return index.get(IDBKeyRange.only(loginObject.userId));
-        }).then(function (foundObject) {
-            if (foundObject && (foundObject.userId==loginObject.userId &&
-                foundObject.password==loginObject.password)){
-                console.log("login successful");
-            } else {
-                alert("login or password incorrect")
-            }
-        });
-    }
-}
-
-
-/**
- * it retrieves the event data for an event from localStorage
- * @param event
- * @returns {*}
- */
-function getCachedData(event) {
-    const value = localStorage.getItem(event);
-    if (value == null)
-        return {event: event};
-    else return JSON.parse(value);
-}
-
-
-/**
- * given the server data, it returns the value of the field precipitations
- * @param dataR the data returned by the server
- * @returns {*}
- */
-function getTitle(dataR) {
-    if (dataR.title == null && dataR.title === undefined)
-        return "unavailable";
-    return dataR.title
-}
-
-/**
- * given the server data, it returns the value of the field wind
- * @param dataR the data returned by the server
- * @returns {*}
- */
-function getDescription(dataR){
-    if (dataR.description == null && dataR.description === undefined)
-        return "unavailable";
-    else return dataR.description;
-}
-
-/**
- * given the server data, it returns the value of the field temperature
- * @param dataR the data returned by the server
- * @returns {*}
- */
-function getCreator(dataR){
-    if (dataR.creator == null && dataR.creator === undefined)
-        return "unavailable";
-    else return dataR.creator;
-}
-/**
- * given the server data, it returns the value of the field humidity
- * @param dataR the data returned by the server
- * @returns {*}
- */
-function getDate(dataR){
-    if (dataR.date == null && dataR.date === undefined)
-        return "unavailable";
-    else return dataR.date;
 }
--- routes/create-event.js
+++ routes/create-event.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended: false}));
 
 router.get('/', function(req, res, next) {
     res.render('create-event', { title: 'photofest'});
+});
+
+router.post('/', function(req, res, next) {
+    var userData = req.body;
+    if (userData == null) {
+        res.status(403).send('No data sent!')
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(userData));
+    //res.render('/index', { title: 'photofest'})
 });
 
 module.exports = router;
